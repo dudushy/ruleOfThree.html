7023508d717b083f4cf12a4310e95e420d08b905
dudushy <eduardotalarico1337@gmail.com>
2023-09-15 20:57:54 -0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,18 @@
-console.log("[template-web] Hello World!");
+const TITLE = "ruleOfThree.html";
+console.log(`[${TITLE}] init`);
+
+function calculate() {
+    const value1 = document.getElementById("value1").value;
+    console.log(`[${TITLE}#calculate] value1`, value1);
+
+    const result1 = document.getElementById("result1").value;
+    console.log(`[${TITLE}#calculate] result1`, result1);
+
+    const value2 = document.getElementById("value2").value;
+    console.log(`[${TITLE}#calculate] value2`, value2);
+
+    const output = (result1 * value2) / value1;
+    console.log(`[${TITLE}#calculate] output { (${result1} * ${value2}) / ${value1} }`, output);
+
+    document.getElementById("result2").value = output;
+}
